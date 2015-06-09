6dca088df2d2a427a4218d83dc109c0709c28708
Ilya Kolbin <iskolbin@gmail.com>
2015-06-09 21:50:14 +0300

    copypixels crop renderer

--- luce/RendererCopyPixelsCrop.hx
+++ luce/RendererCopyPixelsCrop.hx
@@ -1,11 +1,13 @@
 package luce;
 
 import openfl.geom.Point;
+import openfl.geom.Rect;
 import openfl.display.BitmapData;
 
 class RendererCopyPixelsCrop implements Batch.BatchRenderer {
-	var xy = new Array<Point>();
+	var auxPoint = new Point( 0, 0 );
 	var zeroPoint = new Point( 0, 0 );
+	var auxRect = new Rectangle( 0, 0, 0, 0 );
 	var buffer: BitmapData;
 
 	public function new( buffer: BitmapData ) {
@@ -24,16 +26,43 @@ class RendererCopyPixelsCrop implements Batch.BatchRenderer {
 			if ( id_ != Atlas.NULL ) {
 				var id = Std.int( id_ );
 				var c = batch.atlas.centers[id];
-				var x = batch.getCX( shift ) - c.x;
-				var y = batch.getCY( shift ) - c.y;
+				var xmin = batch.getCX( shift ) - c.x;
+				var ymin = batch.getCY( shift ) - c.y;
+				var xmax = xmin + rect.width;
+				var ymax = ymin + rect.height;
 				var rect = batch.atlas.rects[id];
-				if ( x >= 0 && y >= 0 && x + rect.width < buffer.width && y + rect.height < buffer.height ) {
-					var p = xy[i];
-
-					p.x = batch.getCX( shift ) - c.x;
-					p.y = batch.getCY( shift ) - c.y;
-
-					buffer.copyPixels( batch.atlas.bitmapData, rect, p, null, zeroPoint, true);							
+				auxPoint.x = xmin;
+				auxPoint.y = ymin;
+				if ( xmin >= 0 && ymin >= 0 && xmax < buffer.width && ymax < buffer.height ) {
+					buffer.copyPixels( batch.atlas.bitmapData, rect, auxPoint, null, zeroPoint, true);							
+				} else {
+					auxRect.x = rect.x;
+					auxRect.y = rect.y;
+					auxRect.width = rect.width;
+					auxRect.height = rect.height;
+					if ( xmin < 0 ) {
+						auxRect.x -= xmin;
+						auxRect.width += xmin;
+					} else if ( xmin >= buffer.width ) {
+						continue;
+					}
+					if ( ymin < 0 ) {
+						auxRect.y -= ymin;
+						auxRect.height += ymin;
+					} else if ( ymin >= buffer.height ) {
+						continue;
+					}
+					if ( xmax >= buffer.width ) {
+						auxRect.width -= ( xmax - buffer.width + 1 )
+					} else if ( xmax < 0 ) {
+						continue;
+					}
+					if ( ymax >= buffer.height ) {
+						auxRect.height -= ( ymax - buffer.height + 1 ) 
+					} else if ( ymax < 0 ) {
+						continue;
+					}
+					buffer.copyPixels( batch.atlas.bitmapData, auxRect, auxPoint, null, zeroPoint, true );
 				}
 			}
 			shift += Batch.WGT_SIZE;
